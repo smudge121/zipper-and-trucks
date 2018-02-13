f8a65b3044d239623894d784a5b0cbb5c2bee58f
Jaret Ishii <jaretishii17@gmail.com>
2018-02-12 18:50:11 -0800

    initializing

---
+++ Assets/playerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour {
+
+    public bool lookDirection = true; //right is true, left is false
+    Rigidbody2D rb;
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float x = Input.GetAxisRaw("Horizontal");
+        if (x < 0)
+            lookDirection = false;
+        else if (x > 0)
+            lookDirection = true;
+        rb.velocity = new Vector2(x, 0).normalized * speed;
+    }
+    
+}
