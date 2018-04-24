906465676fe64c3db3e41669b2b8fa96a957523b
Hakan Alpay <hak7alp@gmail.com>
2018-04-23 19:22:17 -0700

    sucking script 2

---
+++ Assets/Scripts/SuckingScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SuckingScript : MonoBehaviour {
+
+	int layer_mask;
+	public Transform playerTrans;
+	// Use this for initialization
+	void Start () {
+		layer_mask = LayerMask.GetMask("InteractableObject");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.M))
+		{
+			RaycastHit2D hit;
+			if(Input.GetAxis("Horizontal") == 1)
+			{
+				hit = Physics2D.Raycast(playerTrans.position, new Vector2(1, 0), 1000f, layer_mask);
+				Debug.Log("cast");
+				if (hit.collider != null)
+				{
+					Destroy(hit.collider.gameObject);
+				}
+			}
+		}
+	}
+}
