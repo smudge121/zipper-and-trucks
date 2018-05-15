e7e1387bb65661e21ebd4c2b05539c44a573e3fd
Hakan Alpay <hak7alp@gmail.com>
2018-05-14 20:26:00 -0700

    Added sprite animators

---
+++ Assets/Scripts/ElephantPouchAnimator.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElephantPouchAnimator : MonoBehaviour
+{
+    public SpriteAnimator spriteAnimator;
+    public PlatformController platformController;
+    public Rigidbody2D rigidbody2D;
+
+    // Use this for initialization
+    void Start()
+    {
+        spriteAnimator = GetComponent<SpriteAnimator>();
+        platformController = transform.root.GetComponent<PlatformController>();
+        rigidbody2D = transform.root.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if she's not grounded and moving up, play jump
+        if (!platformController.grounded && rigidbody2D.velocity.y > 0)
+        {
+            spriteAnimator.Play("Jump");
+        }
+        //if she's not grounded and moving down, play fall
+        else if (!platformController.grounded && rigidbody2D.velocity.y < 0)
+        {
+            spriteAnimator.Play("Fall");
+        }
+        //if she's grounded and moving left or right, play Walk
+        else if (platformController.grounded && (Mathf.Abs(Input.GetAxisRaw("Horizontal")) > 0f))
+        {
+            spriteAnimator.Play("Walk");
+        }
+        //if none of these are the case, play idle
+        else
+        {
+            spriteAnimator.Play("Idle");
+        }
+    }
+}
--- Assets/Scripts/PlatformController.cs
+++ Assets/Scripts/PlatformController.cs
@@ -3,39 +3,39 @@
 public class PlatformController : MonoBehaviour
 {
     private Rigidbody2D rb;
-    
+
     private CapsuleCollider2D collider;
     private SpriteAnimator spriteAnimator;
 
     [Header("Movement")]
-    
+
     public bool canJump = true;
 
     public ElephantState eleState;
-    
+
     public float runSpeed = 8;
     private float xMove;
     public float maxSpeed = 5f;
-    
+
     [HideInInspector] public bool facingRight;
-    
+
     [Space(10)]
-    
+
     [Header("Jumping")]
-    
+
     [HideInInspector] public bool jump;
     public float fallMultiplier = 2.5f;
     public float jumpForce = 1000f;
     public float lowJumpMultiplier = 2f;
 
     [Space(10)]
-    
-    [Header("Grounding")]
-    
-    private bool grounded;
-    public LayerMask groundLayers;  
 
-    
+    [Header("Grounding")]
+
+    public bool grounded;
+    public LayerMask groundLayers;
+
+
     private Vector3 topLeft;
     private Vector3 btmRight;
     private Vector3 topRightOffset;
@@ -47,7 +47,7 @@ public class PlatformController : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         collider = GetComponent<CapsuleCollider2D>();
-        
+
         setUpCorners();
     }
 
@@ -62,7 +62,7 @@ public class PlatformController : MonoBehaviour
         else if (Input.GetAxisRaw("Horizontal") == 1 && facingRight)
             Flip();
 
-        
+
 
         //mario jump
         if (rb.velocity.y < 0)
@@ -76,8 +76,8 @@ public class PlatformController : MonoBehaviour
         //check if grounded
 //        grounded = Physics2D.OverlapArea(topLeft.position, bottomRight.position, groundLayers);
         grounded = isGrounded();
-        
-        
+
+
         float h;
 
         //store Right Hor input
@@ -105,34 +105,34 @@ public class PlatformController : MonoBehaviour
     //on start and rotate character, set up top and bottom boxes for ceiling/ground detection and raycast for turning
     private void setUpCorners()
     {
-        
-        
+
+
         //get extents
         float top = collider.offset.y + (collider.size.y / 2f);
         float btm = collider.offset.y - (collider.size.y / 2f);
         float left = collider.offset.x - (collider.size.x / 2f);
         float right = collider.offset.x + (collider.size.x /2f);
-           
+
         //get corners
         topLeft = (new Vector2( left, top));
         btmRight = (new Vector2( right, btm));
-        
+
         //ceiling detect will use topLeft to topRight + offset
         topRightOffset = (new Vector2( right, top + 0.1f));
-        
+
         //ground detect will use btmRight to btmLeft - offset
         btmLeftOffset = (new Vector2( left, btm - 0.1f));
-        
+
 
     }
-    
+
     //determine if grounded by current set up
     private bool isGrounded()
     {
         Debug.DrawLine(transform.position +btmRight, transform.position +btmLeftOffset);
         return Physics2D.OverlapArea(transform.position + btmRight, transform.position + btmLeftOffset, groundLayers);
     }
-    
+
     private void Flip()
     {
         facingRight = !facingRight;
